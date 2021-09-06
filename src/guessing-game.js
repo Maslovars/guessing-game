4408a2ca234e731d51df3662951df31af4a8268f
Maslovars <maslovars@gmail.com>
2021-09-07 00:05:22 +0300

    feat: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return (this.min + Math.ceil((this.max - this.min) / 2));
     }
 
     lower() {
-
+        this.setRange(this.min, this.guess());
+        this.guess();
     }
 
     greater() {
-
+        this.setRange(this.guess(), this.max);
+        this.guess();
     }
 }
 
